9fb9283182a0458f6dbda2cb4df4b49bb81fbb71
Drew DeVault <sir@cmpwn.com>
2013-02-02 14:58:08 -0700

    Added duplication tool

---
+++ README.md
@@ -0,0 +1,50 @@
+# Subreddit Utilities
+
+This is a tool for working with subreddits you moderate on [Reddit](http://reddit.com). It allows you to easily perform
+a variety of tasks, such as resetting your subreddit settings to defaults, or copying one subreddit to another.
+
+## How to use
+
+If you are on Linux or Mac, you will need to install [Mono](http://mono-project.com) to use this tool. Preface all commands
+with `mono` on these platforms. On Windows, you will need the .NET 4.0 Framework, which you may already have installed.
+
+    srutil [command] [arguments...]
+
+You may call srutil and provide it a command you wish to perform from the list below. All of these commands take a unique
+set of arguments based on what the command does.
+
+## Commands
+
+### reset
+
+Resets a subreddit's settings and style to the default.
+
+    srutil reset /r/example [filters...]
+
+*Aliases*: clear
+
+The filters determine which parts of the subreddit are reset. It's a comma-delimited list of filters.
+
+Valid filters include: css,images,settings,sidebar,flair
+
+You may also specify "all".
+
+**Examples**
+
+To completely reset a subreddit to default: `srutil reset /r/example all`
+
+To reset CSS and images: `srutil reset /r/example css,images`
+
+To remove all flair: `srutil reset /r/example flair`
+
+### duplicate
+
+Duplicates the styles and settings of one subreddit into another.
+
+    srutil duplicate /r/from /r/to
+
+*Aliases*: dup dupe copy
+
+All settings and styles will be reset in /r/to, then /r/from copied into /r/to.
+
+You must be a moderator of /r/to, but needn't moderate /r/from.
--- srutil/Duplicate.cs
+++ srutil/Duplicate.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using RedditSharp;
 
@@ -10,12 +11,61 @@ namespace srutil
     {
         public string[] GetOperationNames()
         {
-            return new[] { "dup", "dupe", "duplicate" };
+            return new[] { "dup", "dupe", "duplicate", "copy" };
         }
 
         public void Execute(string[] args, Reddit reddit)
         {
-            
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Invalid usage. See 'srutil help dupe' for more information.");
+                return;
+            }
+            Console.WriteLine("Resetting {0} to default settings...", args[2]);
+            var reset = new Reset();
+            reset.Execute(new[] { "reset", args[2], "all" }, reddit);
+            // TODO: Better error handling
+            var from = reddit.GetSubreddit(args[1]);
+            var to = reddit.GetSubreddit(args[2]);
+            Console.WriteLine("Copying {0} into {1}...", from.DisplayName, to.DisplayName);
+
+            // Copy settings
+            Console.WriteLine("Copying settings...");
+            var settings = from.GetSettings();
+            settings.Subreddit = to;
+            settings.UpdateSettings();
+            Console.WriteLine("Done.");
+
+            // Copy images
+            Console.WriteLine("Copying images...");
+            var stylesFrom = from.GetStylesheet();
+            var styleTo = to.GetStylesheet();
+
+            var webClient = new WebClient();
+            int left = Console.CursorLeft; int top = Console.CursorTop;
+            int progress = 1;
+            foreach (var image in stylesFrom.Images)
+            {
+                var data = webClient.DownloadData(image.Url);
+                styleTo.UploadImage(image.Name, ImageType.PNG, data);
+                Console.CursorLeft = left; Console.CursorTop = top;
+                Console.WriteLine("{0}/{1}", progress++, stylesFrom.Images.Count);
+            }
+            Console.WriteLine("Done.");
+
+            // Copy styles
+            Console.WriteLine("Copying styles...");
+            styleTo.CSS = stylesFrom.CSS;
+            styleTo.UpdateCss();
+            Console.WriteLine("Done.");
+
+            // Header
+            Console.WriteLine("Copying header image...");
+            var headerImage = webClient.DownloadData(from.HeaderImage);
+            to.UploadHeaderImage("header", ImageType.PNG, headerImage);
+            Console.WriteLine("Done.");
+
+            Console.WriteLine("Copied {0} to {1}.", from.DisplayName, to.DisplayName);
         }
 
         public void ShowHelp()
